6d450b565403205b55698ecd32fb688bb6e03585
pedromssoares <pedromiguel_s@hotmail.com>
2021-04-30 13:39:45 -0300

    Feature: load data from pokeapi

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,12 +1,16 @@
 import {Component, Input} from '@angular/core';
-import {Pokemon} from '../../_model/Pokemon';
+import {getPokemonImage, getPokemonNumber, Pokemon} from '../../_model/Pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
-  styleUrls: ['./pokemon-card.component.sass']
+  styleUrls: ['./pokemon-card.component.sass'],
 })
 export class PokemonCardComponent {
   @Input()
   public pokemon: Pokemon;
+
+  public getPokemonImage = getPokemonImage;
+
+  public getPokemonNumber = getPokemonNumber;
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,55 +1,15 @@
 import {Component} from '@angular/core';
-import {Pokemon} from '../../_model/Pokemon';
-import {Type} from '../../_model/Type';
+import {PokemonService} from '../../_services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.sass']
+  styleUrls: ['./pokemon-list.component.sass'],
 })
 export class PokemonListComponent {
 
-  public pokemons: Pokemon[] = [
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
-      number: 1,
-      name: 'Bulbasaur',
-      types: [
-        Type.Grass,
-        Type.Poison,
-      ]
-    },
-
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png',
-      number: 2,
-      name: 'Ivysaur',
-      types: [
-        Type.Grass,
-        Type.Poison,
-      ]
-    },
-
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',
-      number: 3,
-      name: 'Venusaur',
-      types: [
-        Type.Grass,
-        Type.Poison,
-      ]
-    },
-
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
-      number: 4,
-      name: 'Charmander',
-      types: [
-        Type.Fire,
-        Type.Poison,
-      ]
-    }
-  ];
-
+  constructor(
+    public pokemonService: PokemonService,
+  ) {
+  }
 }
-
