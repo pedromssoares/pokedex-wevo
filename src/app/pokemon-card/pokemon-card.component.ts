6121a6871954d0a192841847750072fc62abfdb1
pedromssoares <pedromiguel_s@hotmail.com>
2021-04-30 11:23:21 -0300

    Feature: pokemon data

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,16 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Pokemon} from '../../_model/Pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.sass']
 })
-export class PokemonCardComponent implements OnInit {
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
-
+export class PokemonCardComponent {
+  @Input()
+  public pokemon: Pokemon;
 }
